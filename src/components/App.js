47ae86b368284b97c1971e5ce1aab4d804e7dfa3
Nikhil <ns345360@gmail.com>
2022-12-12 20:42:30 +0530

    created actions, reducer for adding movies to the `list` inside our store and also the rendered the movies inside `list`

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+
 import { data } from "../data";
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
+import { addMovies } from "../actions";
 
-function App() {
+function App(props) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    //when store updated, then we run this code...
+    props.store.subscribe(() => {
+      console.log("STORE UPDATED");
+      console.log(props.store.getState().list);
+      const { list } = props.store.getState();
+      setMovies(list);
+    });
+
+    //dispatching the action to add the movies to the store
+    props.store.dispatch(addMovies(data));
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
@@ -13,8 +31,9 @@ function App() {
         </div>
 
         <div className="list">
-          {data.map((movie) => {
-            return <MovieCard movie={movie} />;
+          {/* index parameter is provided by the map function itself */}
+          {movies.map((movie, index) => {
+            return <MovieCard movie={movie} key={`movie-${index}`} />;
           })}
         </div>
       </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,18 @@
-//state is the current state inside the store, default value has been set to empty array
-export default function movies(state = [], action) {
-  if (action.type === "ADD_MOVIES") {
-    return action.movies;
+import { ADD_MOVIES } from "../actions";
+
+const initialMoviesState = {
+  list: [],
+  favourites: [],
+};
+
+//state is the current state(state before executing the action) inside the store
+//default value has been given for the case if the state has not yet been made inside the store(that is, their is no state named 'movies' inside the store initially)
+export default function movies(state = initialMoviesState, action) {
+  if (action.type === ADD_MOVIES) {
+    return {
+      ...state,
+      list: action.movies,
+    };
   }
   return state; //return the current state only, if no new state created
 }
